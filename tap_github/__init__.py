f0f2603e0cc03aeafa3034813fdeaa0738a68590
vitaliizazmic <vitalii.vdovenko@zazmic.com>
2021-03-30 16:07:24 +0300

    Handling errors while syncing Collaborators stream

--- tap_github/__init__.py
+++ tap_github/__init__.py
@@ -767,19 +767,22 @@ def get_all_collaborators(schema, repo_path, state, mdata):
     https://developer.github.com/v3/repos/collaborators/#list-collaborators
     '''
     with metrics.record_counter('collaborators') as counter:
-        for response in authed_get_all_pages(
-                'collaborators',
-                'https://api.github.com/repos/{}/collaborators'.format(repo_path)
-        ):
-            collaborators = response.json()
-            extraction_time = singer.utils.now()
-            for collaborator in collaborators:
-                collaborator['_sdc_repository'] = repo_path
-                with singer.Transformer() as transformer:
-                    rec = transformer.transform(collaborator, schema, metadata=metadata.to_map(mdata))
-                singer.write_record('collaborators', rec, time_extracted=extraction_time)
-                singer.write_bookmark(state, repo_path, 'collaborator', {'since': singer.utils.strftime(extraction_time)})
-                counter.increment()
+        try:
+            for response in authed_get_all_pages(
+                    'collaborators',
+                    'https://api.github.com/repos/{}/collaborators'.format(repo_path)
+            ):
+                collaborators = response.json()
+                extraction_time = singer.utils.now()
+                for collaborator in collaborators:
+                    collaborator['_sdc_repository'] = repo_path
+                    with singer.Transformer() as transformer:
+                        rec = transformer.transform(collaborator, schema, metadata=metadata.to_map(mdata))
+                    singer.write_record('collaborators', rec, time_extracted=extraction_time)
+                    singer.write_bookmark(state, repo_path, 'collaborator', {'since': singer.utils.strftime(extraction_time)})
+                    counter.increment()
+        except (NotFoundException, AuthException):
+            logger.warning(f"Syncing Collaborators stream isn't available for repository {repo_path}")
 
     return state
 
